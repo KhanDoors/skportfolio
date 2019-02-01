6d3c3b0cfa6f97ac859f8029d233a604ba468830
unknown <syyadkhan@yahoo.com>
2019-02-01 18:36:46 -0500

    navbar work

--- src/components/globals/navbar/NavbarHeader.js
+++ src/components/globals/navbar/NavbarHeader.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
+import { Link } from 'gatsby'
+import logo12 from '../../../images/logo12.png'
+import { FaAlignRight } from 'react-icons/fa'
+import styled from 'styled-components'
 
 class NavbarHeader extends Component {
   render() {
-    return <div>Navbar Header</div>
+    const { handleNavbar } = this.props
+    return (
+      <HeaderWrapper>
+        <Link to="/">
+          <img src={logo12} alt="the logo" />
+        </Link>
+        <FaAlignRight
+          className="toggle-icon"
+          onClick={() => {
+            handleNavbar()
+          }}
+        />
+      </HeaderWrapper>
+    )
   }
 }
+
+const HeaderWrapper = styled.div`
+  padding: 0.4rem 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  .toggle-icon {
+    font-size: 1.75rem;
+    color: yellow;
+    cursor: pointer;
+  }
+  @media (min-width: 768px) {
+    .toggle-icon {
+      display: none;
+    }
+  }
+`
 
 export default NavbarHeader
