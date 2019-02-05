b961acb99dfb25514dfc1ccc24e1b1266422033d
unknown <syyadkhan@yahoo.com>
2019-02-04 20:35:34 -0500

    small change

--- src/utils/Banner.js
+++ src/utils/Banner.js
@@ -14,6 +14,7 @@ const Banner = ({ title, subtitle, children }) => {
 
 const BannerWrapper = styled.div`
   margin-bottom: 3rem;
+  text-align: center;
   .title {
     color: ${styles.colors.mainRed};
     font-size: 3rem;
