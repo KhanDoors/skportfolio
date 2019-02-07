68456efb1ede373a84205fdf30fe1e40592f2636
unknown <syyadkhan@yahoo.com>
2019-02-06 21:11:24 -0500

    section component

---
+++ src/components/HomePageComponents/QuickInfo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Section, Title } from '../../utils'
+
+class QuickInfo extends Component {
+  render() {
+    return (
+      <Section>
+        <Title message="quick look at what i'm into" title="My Tech" />
+      </Section>
+    )
+  }
+}
+
+export default QuickInfo
---
+++ src/utils/Title.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+import { styles } from '../utils'
+
+const Title = ({ title, message }) => {
+  return (
+    <TitleWrapper>
+      <h3 className="message">{message}</h3>
+      <h1 className="title">{title}</h1>
+      <div className="underline" />
+    </TitleWrapper>
+  )
+}
+
+Title.defualtPorps = {
+  message: 'our message',
+  title: 'our title',
+}
+
+const TitleWrapper = styled.div`
+  text-align: center;
+  .message {
+    ${styles.textSlanted};
+    ${styles.letterSpacing({ spacing: '.3rem' })}
+    font-size:2rem;
+    color: ${styles.colors.mainGrey};
+  }
+  .title {
+    ${styles.letterSpacing({ spacing: '.3rem' })};
+    font-size: 2rem;
+    text-transform: uppercase;
+    color: ${styles.colors.mainRed};
+  }
+  .underline {
+    width: 5rem;
+    height: 0.2rem;
+    background: ${styles.colors.mainBurg};
+    margin: 0.5rem auto;
+  }
+`
+
+export default Title
