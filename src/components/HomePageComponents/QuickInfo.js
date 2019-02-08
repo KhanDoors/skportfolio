36f28f71744c1bcb2592803f8e382cdd56b5e225
unknown <syyadkhan@yahoo.com>
2019-02-07 19:37:51 -0500

    more buttonz

--- src/components/HomePageComponents/QuickInfo.js
+++ src/components/HomePageComponents/QuickInfo.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { Section, Title } from '../../utils'
+import { Section, Title, SectionButton } from '../../utils'
 
 class QuickInfo extends Component {
   render() {
     return (
       <Section>
         <Title message="quick look at what i'm into" title="My Tech" />
+        <SectionButton>Contact</SectionButton>
       </Section>
     )
   }
