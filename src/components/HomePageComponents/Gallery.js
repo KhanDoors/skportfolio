e4f070f2b2e9d54e9a77af6d1be1d26d4823bb58
unknown <syyadkhan@yahoo.com>
2019-02-09 15:06:44 -0500

    image action graphql

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -4,23 +4,88 @@ import { StaticQuery, graphql } from 'gatsby'
 import { styles, Section } from '../../utils'
 import Img from 'gatsby-image'
 
+const SINGLE_IMAGE = graphql`
+  {
+    img1: file(relativePath: { eq: "docker.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    img2: file(relativePath: { eq: "Express.JS-nodejs.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    img3: file(relativePath: { eq: "graphql.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    img4: file(relativePath: { eq: "gatsby-icon.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
 const Gallery = () => {
   return (
     <StaticQuery
-      query={graphql`
-        {
-          img1: file(relativePath: { eq: "docker.png" }) {
-            childImageSharp {
-              fluid(maxWidth: 500) {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-        }
-      `}
-      render={data => <Img fluid={data.img1.childImageSharp.fluid} />}
+      query={SINGLE_IMAGE}
+      render={data => {
+        const img1 = data.img1.childImageSharp.fluid
+        const img2 = data.img2.childImageSharp.fluid
+        const img3 = data.img3.childImageSharp.fluid
+        const img4 = data.img4.childImageSharp.fluid
+        return (
+          <Section>
+            <GalleryWrapper>
+              <div className="item item-1">
+                <Img fluid={img1} />
+              </div>
+              <div className="item item-2">
+                <Img fluid={img2} />
+              </div>
+              <div className="item item-3">
+                <Img fluid={img3} />
+              </div>
+              <div className="item item-3">
+                <Img fluid={img4} />
+              </div>
+            </GalleryWrapper>
+          </Section>
+        )
+      }}
     />
   )
 }
+
+const GalleryWrapper = styled.div`
+  display: grid;
+  grid-template-columns: auto;
+  grid-row-gap: 1rem;
+  .item {
+    position: relative;
+  }
+  @media (min-width: 576px) {
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 1rem;
+  }
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+`
 
 export default Gallery
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { PageHeader, Banner } from '../utils'
-import aboutImg from '../images/aboutPage.jpeg'
+import aboutImg from '../images/aboutPage.jpg'
 
 const AboutPage = () => (
   <Layout>
