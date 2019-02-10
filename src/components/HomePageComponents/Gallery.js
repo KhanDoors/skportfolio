f10b0a1ebd63b1ae3c154f39a7123ff82e93bef3
unknown <syyadkhan@yahoo.com>
2019-02-10 11:32:44 -0500

    images and refactoring

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -4,75 +4,16 @@ import { StaticQuery, graphql } from 'gatsby'
 import { styles, Section } from '../../utils'
 import Img from 'gatsby-image'
 
-const SINGLE_IMAGE = graphql`
+const GET_IMAGES = graphql`
   {
-    img1: file(relativePath: { eq: "homeGallery/docker.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 500, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    img2: file(relativePath: { eq: "homeGallery/Express.JS-nodejs.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 500, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    img3: file(relativePath: { eq: "homeGallery/graphql.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 500, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    img4: file(relativePath: { eq: "homeGallery/gatsby.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 500, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    img5: file(relativePath: { eq: "homeGallery/htmlcss.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 500, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    img6: file(relativePath: { eq: "homeGallery/gimp.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 500, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    img7: file(relativePath: { eq: "homeGallery/adobe-xd.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 500, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    img8: file(relativePath: { eq: "homeGallery/javascript.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 500, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    img9: file(relativePath: { eq: "homeGallery/mongo.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 500, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-    img10: file(relativePath: { eq: "homeGallery/react1.png" }) {
-      childImageSharp {
-        fluid(maxWidth: 500, maxHeight: 250) {
-          ...GatsbyImageSharpFluid
+    getImages: allFile(filter: { relativeDirectory: { eq: "homeGallery" } }) {
+      edges {
+        node {
+          childImageSharp {
+            fluid(maxWidth: 500, maxHeight: 250) {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
     }
@@ -82,51 +23,19 @@ const SINGLE_IMAGE = graphql`
 const Gallery = () => {
   return (
     <StaticQuery
-      query={SINGLE_IMAGE}
+      query={GET_IMAGES}
       render={data => {
-        const img1 = data.img1.childImageSharp.fluid
-        const img2 = data.img2.childImageSharp.fluid
-        const img3 = data.img3.childImageSharp.fluid
-        const img4 = data.img4.childImageSharp.fluid
-        const img5 = data.img5.childImageSharp.fluid
-        const img6 = data.img6.childImageSharp.fluid
-        const img7 = data.img7.childImageSharp.fluid
-        const img8 = data.img8.childImageSharp.fluid
-        const img9 = data.img9.childImageSharp.fluid
-        const img10 = data.img10.childImageSharp.fluid
+        const images = data.getImages.edges
         return (
           <Section>
             <GalleryWrapper>
-              <div className="item item-1">
-                <Img fluid={img1} />
-              </div>
-              <div className="item item-2">
-                <Img fluid={img2} />
-              </div>
-              <div className="item item-3">
-                <Img fluid={img3} />
-              </div>
-              <div className="item item-4">
-                <Img fluid={img4} />
-              </div>
-              <div className="item item-5">
-                <Img fluid={img5} />
-              </div>
-              <div className="item item-6">
-                <Img fluid={img6} />
-              </div>
-              <div className="item item-7">
-                <Img fluid={img7} />
-              </div>
-              <div className="item item-8">
-                <Img fluid={img8} />
-              </div>
-              <div className="item item-9">
-                <Img fluid={img9} />
-              </div>
-              <div className="item item-10">
-                <Img fluid={img10} />
-              </div>
+              {images.map(({ node }, index) => {
+                return (
+                  <div key={index} className={`item item-${index + 1}`}>
+                    <Img fluid={node.childImageSharp.fluid} />
+                  </div>
+                )
+              })}
             </GalleryWrapper>
           </Section>
         )
@@ -150,7 +59,7 @@ const GalleryWrapper = styled.div`
     grid-template-columns: repeat(3, 1fr);
   }
   @media (min-width: 992px) {
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(6, 1fr);
   }
 `
 
