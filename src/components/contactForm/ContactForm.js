4a75a5a3e678da509a5c22a065346e74078d56a3
unknown <syyadkhan@yahoo.com>
2019-02-16 17:29:32 -0500

    got it

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,52 +10,81 @@ const ContactForm = () => {
         message="Call, Message or send an email"
         title="Let's Talk About it"
       />
-      <form action="https://formspree.io/syyadkhan@yahoo.com" method="POST">
-        {/* name */}
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            className="form-control"
-            name="name"
-            id="name"
-            placeholder="john smith"
-          />
-        </div>
+      <FormWrapper>
+        <form action="https://formspree.io/syyadkhan@yahoo.com" method="POST">
+          {/* name */}
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              id="name"
+              placeholder="What's your Name?"
+            />
+          </div>
 
-        {/* email */}
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            className="form-control"
-            name="email"
-            id="email"
-            placeholder="email@email.com"
-          />
-        </div>
+          {/* email */}
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              name="email"
+              id="email"
+              placeholder="Enter Email Please"
+            />
+          </div>
 
-        {/* description */}
-        <div className="form-group">
-          <label htmlFor="description">Description</label>
-          <textarea
-            name="description"
-            id="description"
-            className="form-control"
-            rows="5"
-            placeholder="your description here....."
-          />
-        </div>
+          {/* description */}
+          <div className="form-group">
+            <textarea
+              name="description"
+              id="description"
+              className="form-control"
+              rows="5"
+              placeholder="Tell Me Something Good"
+            />
+          </div>
 
-        <button
-          type="submit"
-          className="btn btn-yellow btn-block text-capitalize mt-5"
-        >
-          Submit
-        </button>
-      </form>
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </FormWrapper>
     </Section>
   )
 }
+
+const FormWrapper = styled.div`
+  width: 90%;
+  margin: 2rem auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background: ${styles.colors.mainWhite};
+  .btn {
+    height: 2rem;
+    width: 5rem;
+    padding: 0.5rem;
+    margin: 0.5rem;
+    background: ${styles.colors.mainGrey};
+    color: ${styles.colors.mainRed};
+    :hover {
+      background: ${styles.colors.mainGreen};
+    }
+  }
+  .form-group input,
+  textarea {
+    width: 30rem;
+    padding: 0.5rem;
+    margin: 0.5rem;
+  }
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+  @media (min-width: 992px) {
+    width: 60%;
+  }
+`
 
 export default ContactForm
