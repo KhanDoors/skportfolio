3af51419f70706ebcca6952e4a6e3f4a300db88a
unknown <syyadkhan@yahoo.com>
2019-02-16 15:44:46 -0500

    cx fingers

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -13,33 +13,34 @@ const ContactForm = () => (
       <form
         className="contactForm"
         name="contact"
-        method="POST"
+        method="post"
         data-netlify="true"
         data-netlify-recaptcha="true"
       >
-        <p>
-          <label>
-            <input type="text" name="name" placeholder="Your Name:" />
-          </label>
-        </p>
-        <p>
-          <label>
-            <input type="email" name="email" placeholder="Your Email:" />
-          </label>
-        </p>
-
-        <p>
-          <label>
-            <textarea name="message" rows="7" placeholder="Message:" />
-          </label>
-        </p>
+        <div className="field half first">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+        </div>
+        <div className="field half">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" />
+        </div>
+        <div className="field">
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" rows="6" />
+        </div>
         <p>
           <input type="file" name="myfile" placeholder="Upload File" />
         </p>
         <div data-netlify-recaptcha="true" />
-        <p>
-          <button type="submit">Send</button>
-        </p>
+        <ul className="actions">
+          <li>
+            <input type="submit" value="Send Message" className="special" />
+          </li>
+          <li>
+            <input type="reset" value="Clear" />
+          </li>
+        </ul>
       </form>
     </FormWrapper>
   </Section>
