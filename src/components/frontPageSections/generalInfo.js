403cf050ff552ea16d1fea3791bb9307dd55ee63
syyad khan <syyadkhan@yahoo.com>
2019-08-04 19:55:50 -0400

    better english

--- src/components/frontPageSections/generalInfo.js
+++ src/components/frontPageSections/generalInfo.js
@@ -10,19 +10,19 @@ class GeneralInfo extends Component {
       <Section>
         <Title
           title="About Me"
-          message="This site will be under construction for a long time to come, hopefully"
+          message="This site will be under construction for a long time to come ... hopefully"
         />
         <GeneralInfoWrapper>
           <h4 className="text">
             Throughout this site you will see what I hope is an honest portrayal
-            of who I am as a developer. The tech that I am familiar with, the
-            projects I have built and the passion I have for this work. I hope
-            to contribute to the blog as frequently as I can. I want to use the
-            Stack page to teach what I know about those techs. I have many more
-            projects to display. I have plans for a Youtube channel, more to
-            come on that. Generally I want to take writing code as far as I
-            possibly can. The Contact page has a form that will email me
-            directly if you need to get in touch with me. Thank you.
+            of who I am as a developer. You will see the tech that I am familiar
+            with, the projects I have built and the passion I have for this
+            work. I hope to contribute to the blog as frequently as I can. I
+            want to use the Stack page to teach what I know about those techs. I
+            have many more projects to display. I have plans for a Youtube
+            channel, more to come on that. Generally I want to take writing code
+            as far as I possibly can. The Contact page has a form that will
+            email me directly if you need to get in touch with me. Thank you.
             <br />
             <br />
             ps. yes some of the transitions and animations are over the top but
